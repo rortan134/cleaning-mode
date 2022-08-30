fe33f109ddf82152f692d083a7fc37d75e1e8804
Rortan <samaritanojr006@gmail.com>
2022-08-31 00:41:13 +0200

    feat: animation renderer

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,6 +1,6 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'ipc-example';
+export type Channels = 'get-colors' | 'activate' | 'backdrop';
 
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
@@ -18,4 +18,16 @@ contextBridge.exposeInMainWorld('electron', {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
     },
   },
+});
+
+ipcRenderer.on('show', () => {
+  const root = document.getElementById('root') as HTMLElement;
+  root.style.animation =
+    'show 0.25s cubic-bezier(0.215, 0.610, 0.355, 1.000) forwards';
+});
+
+ipcRenderer.on('hide', () => {
+  const root = document.getElementById('root') as HTMLElement;
+  root.style.animation =
+    'hide 0.1s cubic-bezier(0.215, 0.610, 0.355, 1.000) forwards';
 });
